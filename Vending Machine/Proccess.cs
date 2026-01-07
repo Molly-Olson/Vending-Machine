d3f58d56dba352b9f6856ba1c37135ee8d5f190b
CodyBonez <CodyBonez@yahoo.com>
2026-01-07 15:40:17 -0800

    Created menu for vending machine app.
    Added proccess class to hold the menu.

---
+++ Vending Machine/Proccess.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vending_Machine
+{
+    public static class Proccess
+    {
+
+
+
+      
+
+        public static void VendingMachineMenu()
+        {
+            string itemName ="";
+                int vendingNumber = 0;
+                bool isSelected = false;
+            do
+            {
+                Console.WriteLine("Select number for vending machine 1 = sodas, 2 = Chips, and 3 = Candy");
+                vendingNumber = Int32.Parse(Console.ReadLine());
+                switch (vendingNumber)
+                {
+                    case 1:
+                        while (isSelected == false)
+                        {
+                            Console.WriteLine("\nSelect soda [CocaCola, Pepsi, RootBeer]");
+                            itemName = Console.ReadLine();
+                            if (itemName.Equals("CocaCola", StringComparison.OrdinalIgnoreCase))
+                            {
+                                Console.WriteLine("Enjoy your CocaCola");
+                                isSelected = true;
+                            }
+                            else if (itemName.Equals("Pepsi", StringComparison.OrdinalIgnoreCase))
+                            {
+                                Console.WriteLine("Enjoy your Pepsi");
+                                isSelected = true;
+                            }
+                            else if (itemName.Equals("RootBeer", StringComparison.OrdinalIgnoreCase))
+                            {
+                                Console.WriteLine("Enjoy your RootBeer");
+                                isSelected = true;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Not the correct soda in the vending machine, please select the correct one you want.");
+                                isSelected = false;
+                            }
+                        }
+                        break;
+
+                    case 2:
+                        while (isSelected == false)
+                        {
+                            Console.WriteLine("\nSelect chips [Lays, Pringles, SunChips]");
+                            itemName = Console.ReadLine();
+                            if (itemName.Equals("Lays", StringComparison.OrdinalIgnoreCase))
+                            {
+                                Console.WriteLine("Enjoy your Lays");
+                                isSelected = true;
+                            }
+                            else if (itemName.Equals("Pringles", StringComparison.OrdinalIgnoreCase))
+                            {
+                                Console.WriteLine("Enjoy your Pringles");
+                                isSelected = true;
+                            }
+                            else if (itemName.Equals("SunChips", StringComparison.OrdinalIgnoreCase))
+                            {
+                                Console.WriteLine("Enjoy your SunChips");
+                                isSelected = true;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Not the correct chips in the vending machine, please select the correct one you want.");
+                                isSelected = false;
+                            }
+                        }
+                        break;
+                    case 3:
+
+                        while (isSelected == false)
+                        {
+                            Console.WriteLine("\nSelect Candy [Snickers, Twix, Reeses]");
+                            itemName = Console.ReadLine();
+                            if (itemName.Equals("Snickers", StringComparison.OrdinalIgnoreCase))
+                            {
+                                Console.WriteLine("Enjoy your Snickers");
+                                isSelected = true;
+                            }
+                            else if (itemName.Equals("Twix", StringComparison.OrdinalIgnoreCase))
+                            {
+                                Console.WriteLine("Enjoy your Twix");
+                                isSelected = true;
+                            }
+                            else if (itemName.Equals("Reeses", StringComparison.OrdinalIgnoreCase))
+                            {
+                                Console.WriteLine("Enjoy your Reeses");
+                                isSelected = true;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Not the correct candy in the vending machine, please select the correct one you want.");
+                                isSelected = false;
+                            }
+                        }
+
+
+
+
+
+                        break;
+                    default:
+
+                        Console.WriteLine("Not in the vending machine, please try again.");
+                        break;
+
+                }
+            } while (vendingNumber != 1 || vendingNumber != 2 || vendingNumber != 3);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Vending Machine/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.Design;
+using System.Runtime.CompilerServices;
+
+namespace Vending_Machine
+{
+    public class Program
+    {
+
+        static void Main(string[] args) {
+
+
+            Proccess.VendingMachineMenu();
+        
+        }
+        
+        
+
+    }
+}
