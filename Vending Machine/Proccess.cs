33c219c39f4edc2b14b4da9580687e1bbe7a0724
Molly Olson <molly.olson@student.centralia.edu>
2026-02-02 15:20:57 -0800

    Cody, How are we to select the type of chips sodas or candy my friend? Finally added week 4 stuff I THINK

--- Vending Machine/Proccess.cs
+++ Vending Machine/Proccess.cs
@@ -3,27 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//Molly was here
+using System.IO;
+//is any of this below needed now that I am not trying to use ILogger?
+using System.Diagnostics;
+using System.Security.Authentication.ExtendedProtection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using System.Security.Cryptography.X509Certificates;
+//end Molly
 
 namespace Vending_Machine
 {
+
     public static class Proccess
     {
-
-
-        public static void VendingMachineMenu()
+        //Molly
+        public class InstanceCounterVend
         {
-            string itemName ="";
+            public int _sodasInvocationCount = 0;
+            public int _ChipsInvocationCount = 0;
+            public int _CandyInvocationCount = 0;
+            public void sodasTrackedFunction()
+            {
+                _sodasInvocationCount++;
+                _ChipsInvocationCount++;
+                _CandyInvocationCount++;
+                Console.WriteLine($"Current Items Vended: {_sodasInvocationCount}, {_ChipsInvocationCount}, {_CandyInvocationCount} time(s). Thank you!");
+            }
+        }
+       
+
+            public static void VendingMachineMenu()
+            {
+            // Molly did things...
+            string writeText = "Vend History"; //add a link to count here
+            File.WriteAllText("filename.txt", writeText);
+
+            string readText = File.ReadAllText("filename.txt");
+            Console.WriteLine(readText);
+
+            
+          
+            //end Molly
+                string itemName = "";
                 int vendingNumber = 0;
                 bool isSelected = false;
-            do
-            {
-                Console.WriteLine("Select number for vending machine 1 = sodas, 2 = Chips, 3 = Candy, and 4 = Exit");
-                vendingNumber = Int32.Parse(Console.ReadLine());
-                switch (vendingNumber)
+                do
                 {
-                    case 1:
-                        
-                        
+                    Console.WriteLine("Select number for vending machine 1 = sodas, 2 = Chips, 3 = Candy, and 4 = Exit");
+                    vendingNumber = Int32.Parse(Console.ReadLine());
+                    switch (vendingNumber)
+                    {
+                        case 1:
+
+
                             Console.WriteLine("\nSelect soda [CocaCola, Pepsi, RootBeer]");
                             itemName = Console.ReadLine();
                             if (itemName.Equals("CocaCola", StringComparison.OrdinalIgnoreCase))
@@ -51,12 +85,12 @@ namespace Vending_Machine
                                 Console.WriteLine("Not the correct soda in the vending machine, going back.");
                                 isSelected = false;
                             }
-                        
-                        break;
 
-                    case 2:
-                    
-                         Console.WriteLine("\nSelect chips [Lays, Pringles, SunChips]");
+                            break;
+
+                        case 2:
+
+                            Console.WriteLine("\nSelect chips [Lays, Pringles, SunChips]");
                             itemName = Console.ReadLine();
                             if (itemName.Equals("Lays", StringComparison.OrdinalIgnoreCase))
                             {
@@ -83,9 +117,9 @@ namespace Vending_Machine
                                 Console.WriteLine("Not the correct chips in the vending machine, going back.");
                                 isSelected = false;
                             }
-                        
-                        break;
-                    case 3:
+
+                            break;
+                        case 3:
 
                             Console.WriteLine("\nSelect Candy [Snickers, Twix, Reeses]");
                             itemName = Console.ReadLine();
@@ -119,21 +153,22 @@ namespace Vending_Machine
                                 Console.WriteLine("Not the correct candy in the vending machine, going back.");
                                 isSelected = false;
                             }
-                        break;
+                            break;
 
 
-                            case 4:
-                        Console.WriteLine("Exiting vending machine");
-                        break;
+                        case 4:
+                            Console.WriteLine("Exiting vending machine");
+                            break;
 
-                    default:
+                        default:
 
-                        Console.WriteLine("Not in the vending machine, please try again.");
-                        break;
+                            Console.WriteLine("Not in the vending machine, please try again.");
+                            break;
 
-                }
-            } while (vendingNumber != 4);
+                    }
+                } while (vendingNumber != 4);
+            }
+
         }
-
     }
-}
+
